42275a6108a65835f3f9737b4a0a7baaccc3c3e9
soheilnazari2001 <soheil.nazari@sharif.edu>
2023-12-18 17:36:56 +0330

    database configurations

--- barnomz/settings.py
+++ barnomz/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "barnomz.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'barnomz',  # Your database name
+        'USER': 'soheil',   # Your PostgreSQL username
+        'PASSWORD': '123456789',  # Your PostgreSQL password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

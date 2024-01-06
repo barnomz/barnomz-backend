f56fbe12a0b3fae42ccd7f81745b0a792adbb41c
soheilnazari2001 <soheil.nazari@sharif.edu>
2024-01-07 01:59:23 +0330

    urls are set

--- barnomz/urls.py
+++ barnomz/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from barnomz_app import views
-from barnomz_app.views import ScheduleList
+from barnomz_app.views import ScheduleList, add_schedule, remove_schedule
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +9,7 @@ urlpatterns = [
     path('api/login/', views.login, name='api_login'),
     path('api/logout/', views.logout, name='api_logout'),
     path('api/schedules/', ScheduleList.as_view(), name='schedule_list'),
+    path('api/schedules/', ScheduleList.as_view(), name='schedule_list'),
+    path('api/schedules/', add_schedule, name='add_schedule'),
+    path('api/schedules/<int:schedule_id>/', remove_schedule, name='remove_schedule'),
 ]

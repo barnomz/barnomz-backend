d6046ad635ca30c9c280da9eea50338e81a9da90
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-13 10:10:22 +0330

    add /auth at the beginning of the authentication endpoints in urls.py

--- barnomz/urls.py
+++ barnomz/urls.py
@@ -4,9 +4,9 @@ from barnomz_app.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/register/', register, name='api_register'),
-    path('api/login/', login, name='api_login'),
-    path('api/logout/', logout, name='api_logout'),
+    path('api/auth/register/', register, name='api_register'),
+    path('api/auth/login/', login, name='api_login'),
+    path('api/auth/logout/', logout, name='api_logout'),
     path('api/schedules/', ScheduleList.as_view(), name='schedule_list'),
     path('api/schedules/', ScheduleList.as_view(), name='schedule_list'),
     path('api/schedules/', add_schedule, name='add_schedule'),

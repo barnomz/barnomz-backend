02b6a2645c7f870e8e3bb8b49bf5bbd235c09f78
soheilnazari2001 <soheil.nazari@sharif.edu>
2024-02-02 21:23:22 +0330

    required fields edited

--- barnomz_app/models.py
+++ barnomz_app/models.py
@@ -76,7 +76,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['student_number', 'major']
+    REQUIRED_FIELDS = ['student_number']
 
     def __str__(self):
         return self.username

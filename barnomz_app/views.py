4530d9588a656b5081576d008ea8d35a3323ebe9
soheilnazari2001 <soheil.nazari@sharif.edu>
2024-02-05 12:27:21 +0330

    fix comments

--- barnomz_app/views.py
+++ barnomz_app/views.py
@@ -241,7 +241,8 @@ class AddComment(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RemoveComment(APIView):

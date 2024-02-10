8680c826b5c084ff30a602fdbfe8f9fa12ba8429
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-02-10 11:36:44 +0330

    fix: add fetch courses action to course model in admin.py

--- barnomz_app/admin.py
+++ barnomz_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+import subprocess
 
 admin.site.register(User)
 admin.site.register(Department)
@@ -8,3 +9,20 @@ admin.site.register(Course)
 admin.site.register(ClassSession)
 admin.site.register(Schedule)
 admin.site.register(CommentOnProfessors)
+
+def fetch_courses(modeladmin, request, queryset):
+    script_path = './edu/main.go'
+    command = f'go run {script_path}'
+
+    try:
+        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+        modeladmin.message_user(request, f'Script executed successfully: {output}')
+    except subprocess.CalledProcessError as e:
+        modeladmin.message_user(request, f'Error executing script: {e.output}', level='error')
+
+fetch_courses.short_description = "Fetch courses"
+
+class FetchCoursesModelAdmin(admin.ModelAdmin):
+    actions = [fetch_courses]
+
+admin.site.register(Course, FetchCoursesModelAdmin)
